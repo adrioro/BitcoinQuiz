9c28f4b83c2c09a69b2b8b4894c4e7795a890448
adrioro <artistsvillage@me.com>
2017-08-24 09:47:53 -0400

    .xml completed. .java two methods: calculateGrade_Q1 (question 1) and createQuizSummary (grade summary).

--- app/src/main/java/com/example/android/bitcoinquiz/MainActivity.java
+++ app/src/main/java/com/example/android/bitcoinquiz/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.android.bitcoinquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * This app offers 4 questions and delivers a greade.
@@ -13,7 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText answerField;
-    int grade= 0;
+    int grade = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,76 +22,39 @@ public class MainActivity extends AppCompatActivity {
         answerField = (EditText) findViewById(R.id.answer_input);
 
     }
-
-    /**
-     * This method is called when each CheckBox is clicked.
-     */
-    public void questionOne_CheckBoxes(View view) {
         /**
-         * Question 1's first checkbox.
+         * Method calculateGrade_Q1 calculates question 1's total grade (Q1 has 3 CheckBoxes).
+         * @param Q1_CheckBox1Checked is wether CheckBox1 is checked.
+         * @param Q1_CheckBox3Checked is wether CheckBox1 is checked.
+         * return final grade
          */
-        CheckBox question1_CheckBox1 = (CheckBox) findViewById(R.id.Q1_checkbox1);
-        boolean yesQuestion1_CheckBox1 = question1_CheckBox1.isChecked();
-        /**
-         * Question 1's second checkbox.
-         */
-        CheckBox question1_CheckBox2 = (CheckBox) findViewById(R.id.Q1_checkbox2);
-        boolean yesQuestion1_CheckBox2 = question1_CheckBox2.isChecked();
-        /**
-         * Question 1's second checkbox.
-         */
-        CheckBox question1_CheckBox3 = (CheckBox) findViewById(R.id.Q1_checkbox3);
-        boolean yesQuestion1_CheckBox3 = question1_CheckBox3.isChecked();
+
+        private int calculateGrade_Q1(boolean Q1_CheckBox1Checked,boolean Q1_CheckBox3Checked){
+
+            int finalGrade = 0;
+            int CheckBox1 = 0;
+            int CheckBox2 = 0;
+            int CheckBox3 = 0;
 
 
-    }
-    /**
-     * Calculates the grade of each CheckBox value.
-     */
+            if (Q1_CheckBox1Checked) {
+                CheckBox1 = 2;
+            }
 
-    private int calculateGrade_Q1 (boolean yesQuestion1_CheckBox1,boolean yesQuestion1_CheckBox2,boolean yesQuestion1_CheckBox3){
-        int finalGrade =0;
-        int CheckBox1 =0;
-        int CheckBox2 =0;
-        int CheckBox3 =0;
-
-
-        if (yesQuestion1_CheckBox1){
-            CheckBox1 = 2;
+            if (Q1_CheckBox3Checked) {
+                CheckBox3 = 2;
+            }
+            finalGrade = CheckBox1 + CheckBox2 + CheckBox3;
+            return finalGrade;
         }
 
-        if (yesQuestion1_CheckBox3){
-            CheckBox3 =2;
-        }
-        finalGrade = CheckBox1 + CheckBox2 + CheckBox3;
-        return finalGrade;
-    }
-
-
     /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView gradeSummaryTextView = (TextView) findViewById(R.id.results);
-        gradeSummaryTextView.setText(message);
-
-    }
-    /**
-     * Grade summary text.
+     * Quiz SUMMARY text.
      */
 
-        private String createGradeSummary(boolean yesQuestion1_CheckBox1,boolean yesQuestion1_CheckBox2,boolean yesQuestion1_CheckBox3){
+        private String createQuizSummary(int grade, boolean Q1_CheckBox1Checked,boolean Q1_CheckBox3Checked){
 
-            int grade = calculateGrade_Q1(yesQuestion1_CheckBox1,yesQuestion1_CheckBox2,yesQuestion1_CheckBox1);
-            String gradeMessage_Q1 = "Your grade" + grade;
-            return(gradeMessage_Q1);
-    }
-
-
-
-    public void submitGrade(View view){
-        String gradeMessage = createGradeSummary();
-        displayMessage(gradeMessage);
+        String gradeMessage = "\n Your grade: " + calculateGrade_Q1(Q1_CheckBox1Checked,Q1_CheckBox3Checked);
     }
 
 
