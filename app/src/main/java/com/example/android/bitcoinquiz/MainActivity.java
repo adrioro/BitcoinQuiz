1deedfa657bbf3320821b00f11d0d3111d1fedcd
adrioro <artistsvillage@me.com>
2017-08-24 10:24:46 -0400

    .xml completed. .java methods added: calculateGrade_Q1 (question 1) createQuizSummary (grade summary) onClick_gradeQuiz (grade button) displayMessage (summary message)

--- app/src/main/java/com/example/android/bitcoinquiz/MainActivity.java
+++ app/src/main/java/com/example/android/bitcoinquiz/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.android.bitcoinquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * This app offers 4 questions and delivers a greade.
@@ -48,17 +52,51 @@ public class MainActivity extends AppCompatActivity {
             return finalGrade;
         }
 
-    /**
-     * Quiz SUMMARY text.
-     */
+        /**
+         * Quiz SUMMARY text.
+         */
 
         private String createQuizSummary(int grade, boolean Q1_CheckBox1Checked,boolean Q1_CheckBox3Checked){
 
             String gradeMessage = "\n Your grade: " + calculateGrade_Q1(Q1_CheckBox1Checked,Q1_CheckBox3Checked);
             return gradeMessage;
+        }
+
+        /**
+         * This method displays the given text on the screen.
+         */
+        private void displayMessage(String message) {
+        TextView quizSummaryTextView = (TextView) findViewById(R.id.grade_summary);
+            quizSummaryTextView.setText(message);
+
     }
 
+        /**
+         * This method is called when the order button is clicked.
+         */
+        public void onClick_gradeQuiz(View view) {
 
+            CheckBox question1_CheckBox1 = (CheckBox) findViewById(R.id.Q1_checkbox1);
+            boolean yesQuestion1_CheckBox1 = question1_CheckBox1.isChecked();
+
+            Log.v("MainActivity", "Answer to CheckBox 1:" + yesQuestion1_CheckBox1);
+            /**
+             * * Question 1's second checkbox.
+             */
+            CheckBox question1_CheckBox2 = (CheckBox) findViewById(R.id.Q1_checkbox2);
+            boolean yesQuestion1_CheckBox2 = question1_CheckBox2.isChecked();
+            /**
+             * Question 1's second checkbox.
+             */
+            CheckBox question1_CheckBox3 = (CheckBox) findViewById(R.id.Q1_checkbox3);
+            boolean yesQuestion1_CheckBox3 = question1_CheckBox3.isChecked();
+
+            int grade = calculateGrade_Q1(Q1_CheckBox1Checked,Q1_CheckBox3Checked);
+            String gradeMessage = createQuizSummary(grade, Q1_CheckBox1Checked,Q1_CheckBox3Checked);
+            displayMessage(gradeMessage);
+
+
+    }
 
 
 }
